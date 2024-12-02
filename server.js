5e78141dad261b8d5f2c1c8152ec71e9892105b4
Rob Hess <hessro@oregonstate.edu>
2024-12-02 15:51:49 -0800

    Add code from 12/2 lecture.

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+var fs = require("fs")
 var express = require('express')
 var exphbs = require('express-handlebars')
 
@@ -9,6 +10,7 @@ var port = process.env.PORT || 8000
 app.engine('handlebars', exphbs.engine({ defaultLayout: 'main' }))
 app.set('view engine', 'handlebars')
 
+app.use(express.json())
 app.use(express.static('static'))
 
 app.get('/', function (req, res, next) {
@@ -27,6 +29,36 @@ app.get('/people/:person', function (req, res, next) {
     res.status(200).render('photoPage', peopleData[person])
   } else {
     next()
+  }
+})
+
+app.post('/people/:person/addPhoto', function (req, res, next) {
+  console.log("  -- req.body:", req.body)
+  if (req.body && req.body.url && req.body.caption) {
+    var person = req.params.person.toLowerCase()
+    var personData = peopleData[person]
+    if (personData) {
+      personData.photos.push({
+        url: req.body.url,
+        caption: req.body.caption
+      })
+      console.log("  -- personData:", personData)
+      fs.writeFile(
+        __dirname + "/peopleData.json",
+        JSON.stringify(peopleData, null, 2),
+        function (err, result) {
+          if (!err) {
+            res.status(200).send()
+          } else {
+            res.status(500).send("Server error.  Try again soon.")
+          }
+        }
+      )
+    } else {
+      next()
+    }
+  } else {
+    res.status(400).send("Request body must contain url and caption.")
   }
 })
 
